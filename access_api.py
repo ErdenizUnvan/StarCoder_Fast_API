d9ace459797baaedc59b0528b6d7f688a4609201
Erdeniz Ünvan <75572539+ErdenizUnvan@users.noreply.github.com>
2023-06-08 21:56:41 +0300

    Add files via upload
    update

---
+++ access_api.py
@@ -0,0 +1,11 @@
+import requests
+import json
+hf_apikey='Your HUGGING FACE API KEY'
+
+url = "http://127.0.0.1:8000/generate"
+headers = {"Content-Type": "application/json","Authorization": f"Bearer {hf_apikey}"}
+query=input('Type your request: ')
+payload = {"prompt": query}
+response = requests.post(url, headers=headers, data=json.dumps(payload))
+result = response.json()
+print(result[0][1])
